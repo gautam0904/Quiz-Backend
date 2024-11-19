89d4471db24bb871bfd51ac6cddc778840836446
gautam malaviya <103646088+gautam0904@users.noreply.github.com>
2024-11-19 23:47:24 +0530

    Update index.ts

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -2,8 +2,6 @@ import mongoose from 'mongoose';
 import { MSG } from '../constant/message'
 
 export const connectDB = async () =>{
-	console.log(process.env.DBURL); // Check if it's the expected value
-
 	mongoose.connect(`${process.env.DBURL}/${process.env.DBNAME}`)
 	console.log(MSG.DBconnected)
 }
